8e61f333a67fc55072bd63419889090f6185007f
nmacadam <37878073+nmacadam@users.noreply.github.com>
2018-10-09 17:13:13 -0400

    Add files via upload

---
+++ src/SpritePlayer.c
@@ -0,0 +1,78 @@
+#pragma bank 2
+#include "main.h"
+#include "Keys.h"
+#include "SpriteManager.h"
+UINT8 bank_SPRITE_PLAYER = 2;
+
+typedef enum {
+	NORMAL,		//State 0
+	JUMPING 	//State 1
+}PLAYER_STATE;
+
+PLAYER_STATE player_state;
+
+INT16 player_accel_y;
+INT16 max_accel = 60;
+INT8 tile_collision;
+
+void Start_SPRITE_PLAYER() {
+	player_state = NORMAL;
+	player_accel_y = 0;
+}
+
+void Update_SPRITE_PLAYER() {
+
+	switch (player_state)
+	{
+	case NORMAL:
+		Movement();
+
+		if (KEY_TICKED(J_A)) {
+			player_accel_y = -50;
+			player_state = JUMPING;
+		}
+
+		//Check if falling
+		if ((player_accel_y >> 4) > 1) {
+			player_state = JUMPING;
+		}
+		break;
+
+	case JUMPING:
+		Movement();
+		break;
+	}
+	
+	if (player_accel_y < max_accel) {
+		player_accel_y += 2;
+	}
+
+	tile_collision = TranslateSprite(THIS, 0, player_accel_y >> 4);
+
+	if (!tile_collision && delta_time != 0 && player_accel_y < max_accel) { //Do another iteration if there is no collision
+		player_accel_y += 2;
+		tile_collision = TranslateSprite(THIS, 0, player_accel_y >> 4);
+	}
+
+	if (tile_collision) {
+		player_accel_y = 0;
+		if (player_state == JUMPING) {
+			player_state = NORMAL;
+		}
+	}
+}
+
+void Destroy_SPRITE_PLAYER() {
+}
+
+void Movement()
+{
+	if (KEY_PRESSED(J_LEFT)) {
+		tile_collision = TranslateSprite(THIS, -1 << delta_time, 0);
+		SPRITE_SET_VMIRROR(THIS);
+	}
+	if (KEY_PRESSED(J_RIGHT)) {
+		tile_collision = TranslateSprite(THIS, 1 << delta_time, 0);
+		SPRITE_UNSET_VMIRROR(THIS);
+	}
+}
---
+++ src/StateGame.c
@@ -0,0 +1,30 @@
+#pragma bank 2
+#include "main.h"
+UINT8 bank_STATE_GAME = 2;
+UINT8 collision_tiles[] = { 1, 0 };
+
+#include "..\res\src\tiles.h"
+#include "..\res\src\map.h"
+
+#include "ZGBMain.h"
+#include "Scroll.h"
+#include "SpriteManager.h"
+
+void Start_STATE_GAME() {
+	UINT8 i;
+
+	SPRITES_8x16;
+	for(i = 0; i != N_SPRITE_TYPES; ++ i) {
+		SpriteManagerLoad(i);
+	}
+	SHOW_SPRITES;
+
+	scroll_target = SpriteManagerAdd(SPRITE_PLAYER, 50, 50);
+
+	InitScrollTiles(0, 2, tiles, 3);
+	InitScroll(mapWidth, mapHeight, map, collision_tiles, 0, 3);
+	SHOW_BKG;
+}
+
+void Update_STATE_GAME() {
+}
